90f5dc40304bd190be14d42d1eddd3a05acb4037
dominic <dominic.chibamu@inventcommerce.com>
2014-11-11 09:47:50 +0200

    More controller test development

--- src/test/java/com/poop/todo/controller/TodoControllerTest.java
+++ src/test/java/com/poop/todo/controller/TodoControllerTest.java
@@ -1,5 +1,29 @@
 package com.poop.todo.controller;
 
-public class TodoControllerTest {
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import com.poop.todo.config.WebAppContext;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes ={TestContext.class, WebAppContext.class})
+public class TodoControllerTest {
+	
+	@Resource
+	private WebApplicationContext webApplicationContext;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setUp(){
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
 }
