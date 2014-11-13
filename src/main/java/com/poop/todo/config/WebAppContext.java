217d08972dd05308a2c2acdd93b9431cfc2bf850
Ngonidzashe Chibamu <ngoni.chibamu@gmail.com>
2014-11-13 02:54:00 +0200

    More hectic controller testing

--- src/main/java/com/poop/todo/config/WebAppContext.java
+++ src/main/java/com/poop/todo/config/WebAppContext.java
@@ -3,15 +3,26 @@ package com.poop.todo.config;
 import java.util.Properties;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+		"com.poop.todo.controller"
+})
 public class WebAppContext extends WebMvcConfigurerAdapter {
+	
+	private static final String VIEW_RESOLVER_PREFIX = "/WEB-INF/jsp/";
+	private static final String VIEW_RESOLVER_SUFFIX = ".jsp";
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry){
@@ -47,8 +58,8 @@ public class WebAppContext extends WebMvcConfigurerAdapter {
 	public ViewResolver viewResolver(){
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);
-		viewResolver.setPrefix("/WEB-INF/jsp/");
-		viewResolver.setSuffix(".jsp");
+		viewResolver.setPrefix(VIEW_RESOLVER_PREFIX);
+		viewResolver.setSuffix(VIEW_RESOLVER_SUFFIX);
 		return viewResolver;
 	}
 }
--- src/main/java/com/poop/todo/controller/TodoController.java
+++ src/main/java/com/poop/todo/controller/TodoController.java
@@ -3,9 +3,12 @@ package com.poop.todo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +21,9 @@ import com.poop.todo.service.TodoService;
 
 @Controller
 public class TodoController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TodoController.class);
+	private static final String MODEL_ATTRIBUTE_TODO = "todo";
+	private static final String VIEW_TODO_ADD = "todo/add";
 	private final TodoService todoService;
 	
 	private final MessageSource messageSource;
@@ -39,6 +45,15 @@ public class TodoController {
 	public TodoDTO findById(@PathVariable("id") Long id) throws TodoNotFoundException{
 		Todo found = todoService.findById(id);
 		return createDTO(found);
+	}
+	
+	@RequestMapping(value = "/todo/add", method = RequestMethod.GET)
+	public void showAddTodoForm(Model model){
+		LOGGER.debug("Rendering add to-do entry form.");
+		
+		 TodoDTO formObject = new TodoDTO();
+		 model.addAttribute(MODEL_ATTRIBUTE_TODO, formObject);
+		 return VIEW_TODO_ADD;
 	}
 	
 	private List<TodoDTO> createDTOs(List<Todo> models){
