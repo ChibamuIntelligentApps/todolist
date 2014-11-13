2e0172614d63f6865ca7f6478c533efa29fcd221
Ngonidzashe Chibamu <ngoni.chibamu@gmail.com>
2014-11-13 02:08:20 +0200

    Added @WebAppConfiguration

--- src/test/java/com/poop/todo/controller/TodoControllerTest.java
+++ src/test/java/com/poop/todo/controller/TodoControllerTest.java
@@ -1,20 +1,28 @@
 package com.poop.todo.controller;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContext;
+
+import com.poop.todo.config.TestContext;
+import com.poop.todo.config.WebAppContext;
+
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.poop.todo.config.WebAppContext;
-
 import javax.annotation.Resource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes ={TestContext.class, WebAppContext.class})
+@WebAppConfiguration
 public class TodoControllerTest {
 	
 	@Resource
@@ -25,5 +33,11 @@ public class TodoControllerTest {
 	@Before
 	public void setUp(){
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	
+	@Test
+	public void showAddToForm_ShouldCreateFormObjectAndRenderAddTodoForm() throws Exception {
+		mockMvc.perform(get("/todo/add"))
+				.andExpect(status().isOk());
 	}
 }
